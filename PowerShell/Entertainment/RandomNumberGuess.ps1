87d34add1c071e5778263ae671aad6884f5ba0a5
tk427 <34039148+tk427@users.noreply.github.com>
2020-02-07 14:07:23 -0500

    Inital Commit

---
+++ PowerShell/Entertainment/RandomNumberGuess.ps1
@@ -0,0 +1,12 @@
+Do {
+	$num = Get-Random -Minimum 0 -Maximum 100
+    Do {
+        $guess = Read-Host "`nPlease guess a number between 0 & 100"
+        If ($guess -gt $num) {Write-Host "`nThat number is too high" -ForegroundColor magenta}
+        If ($guess -lt $num) {Write-Host "`nThat number is too low" -ForegroundColor magenta}
+        If ($guess -eq $num) {Write-Host "`nThat is Correct!!!!" -ForegroundColor cyan}
+        }
+    While ($guess -ne $num)
+    $again = Read-Host "`nWould you like to play again? (Y/n)"
+    }
+    Until ($again -ne "Y"
