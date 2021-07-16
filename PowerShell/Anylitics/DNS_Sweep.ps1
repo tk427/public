9aedd1865645ba8ef200558a286f186a2dd4a971
tk427 <gtimfox@gmail.com>
2021-07-16 13:50:40 -0400

    DNS Sweep
    Simple script to sweep through IP addresses and resolve them to a name.

---
+++ PowerShell/Anylitics/DNS_Sweep.ps1
@@ -0,0 +1,22 @@
+﻿<#
+
+This script only captures live systems!!!!!!!
+
+#>
+
+Clear-Host
+$last_sn = 1
+
+Do
+    {
+       $IP = "192.168.1."+$last_sn
+       write-host `n "Looking up $IP" -foregroundcolor Yellow
+       Resolve-DnsName $IP
+       $Output = Resolve-DnsName $IP
+       $Output | 
+            Select-Object NameHost, @{Name="IP Address";Expression={ $IP }} |
+            Sort-Object $IP |
+            Export-Csv DNS_Sweep.csv -NoTypeInformation -Append
+        $last_sn ++
+        # start-sleep 1
+    } While ($last_sn -lt 256)
