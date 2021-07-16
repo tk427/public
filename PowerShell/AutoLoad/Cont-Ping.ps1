59403c387c3d6270000b04fa592b3ef5fde65605
tk427 <gtimfox@gmail.com>
2021-07-16 13:50:57 -0400

    Added a timer

---
+++ PowerShell/AutoLoad/Cont-Ping.ps1
@@ -0,0 +1,9 @@
+﻿clear-host
+$a = 0
+DO
+    {
+        $time = get-date
+        write-host `n $time -foregroundcolor yellow
+        Test-Connection google.com
+        start-sleep 15
+    } Until ($a -gt 5)
