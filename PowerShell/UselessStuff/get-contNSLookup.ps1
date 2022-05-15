2afc22662efbeba7f927f8a43ab415deb276eb84
timFox <34039148+tk427@users.noreply.github.com>
2022-05-15 09:39:16 -0400

    Create get-contNSLookup.ps1

---
+++ PowerShell/UselessStuff/get-contNSLookup.ps1
@@ -0,0 +1,10 @@
+﻿# Continous NSLookup of Host
+function get-ContDNS {param($target = $(Throw "Requires a Host Name or IP Address"));
+    while ($true)
+        {
+            Write-Host `n "$(Get-Date)" -foregroundcolor cyan
+            Resolve-DnsName $target
+            Start-Sleep -Milliseconds 1000
+            # Start-Sleep -seconds 1
+       }
+    }
