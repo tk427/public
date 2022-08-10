9a816c91020890c2c57d1b692d528758a11cb6e6
timFox <34039148+tk427@users.noreply.github.com>
2022-08-10 13:08:29 -0400

    d
    dd

---
+++ PowerShell/UselessStuff/Get-Countdown.ps1
@@ -0,0 +1,12 @@
+﻿$i= "true"
+$TargetDate = "8/19/2022 16:00:00"
+
+Do
+{
+    Write-Host "Time until"$TargetDate":"
+    Write-Host "Days, Hours, Minutes, Seconds"
+    New-TimeSpan -Start (Get-Date) -End ([datetime]$TargetDate) | Select-Object Days, Hours, Minutes, Seconds
+    Start-Sleep -Seconds 1
+    Clear-Host
+}
+until ($i -ne "true")
