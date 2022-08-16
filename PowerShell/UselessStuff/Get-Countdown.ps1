a7bcd838389057fdfe5694a8f208a63413d9e0cc
timFox <34039148+tk427@users.noreply.github.com>
2022-08-16 14:35:13 -0400

    Update Get-Countdown.ps1
    Working on display issues to make it look more like a real countdown.

--- PowerShell/UselessStuff/Get-Countdown.ps1
+++ PowerShell/UselessStuff/Get-Countdown.ps1
@@ -2,15 +2,19 @@
     Counts down to a specific date/time
 #>
 
+
 $i= "true"
 $TargetDate = "8/19/2022 16:00:00"
+Clear-Host
 
 Do
 {
+    # Clear-Host
     Write-Host "Time until"$TargetDate":"
-    Write-Host "Days, Hours, Minutes, Seconds"
+    Write-Host " Days, Hours, Minutes, Seconds"
     New-TimeSpan -Start (Get-Date) -End ([datetime]$TargetDate) | Select-Object Days, Hours, Minutes, Seconds
-    Start-Sleep -Seconds 1
+    # Start-Sleep -Seconds 1
+    Start-Sleep -Milliseconds 900
     Clear-Host
 }
 until ($i -ne "true")
