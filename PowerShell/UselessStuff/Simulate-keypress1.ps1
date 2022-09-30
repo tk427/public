c4cc12bf9881ebf4f93536b8b967d5ac733431db
timFox <34039148+tk427@users.noreply.github.com>
2022-09-30 15:38:57 -0400

    Update Simulate-keypress1.ps1
    Fixed syntax error

--- PowerShell/UselessStuff/Simulate-keypress1.ps1
+++ PowerShell/UselessStuff/Simulate-keypress1.ps1
@@ -18,7 +18,7 @@ Do
         "7" {[System.Windows.Forms.SendKeys]::SendWait('{RIGHT}')} # Right Arrow Key
         "8" {[System.Windows.Forms.SendKeys]::SendWait('{UP}')} # Up Arrow Key
         "9" {[System.Windows.Forms.SendKeys]::SendWait('{DOWN}')} # Up Arrow Key
-        Default {[System.Windows.Forms.SendKeys]::SendWait('^{ESC}{ESC}')} # Opens the Windows Menu and closes it
+        Default {[System.Windows.Forms.SendKeys]::SendWait('^{ESC}''{ESC}')} # Opens the Windows Menu and closes it
 
     }
     Start-sleep $wait
