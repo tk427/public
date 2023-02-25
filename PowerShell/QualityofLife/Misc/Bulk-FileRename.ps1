bb46053a72f5a3fe4f4b8bde8b147ec432dc3313
timFox <34039148+tk427@users.noreply.github.com>
2023-02-25 18:50:13 -0500

    Update Bulk-FileRename.ps1
    Started updating script to allow for multiple file extensions at the same time and added console outputs.

--- PowerShell/QualityofLife/Misc/Bulk-FileRename.ps1
+++ PowerShell/QualityofLife/Misc/Bulk-FileRename.ps1
@@ -1,7 +1,18 @@
-﻿$index = 16
-$files = Get-ChildItem *.jpeg
-    ForEach ($file in $files)
-    {
-        rename-item $file -NewName "image_$index.jpeg"
-        $index++    
-    }
+﻿<#
+Bulk renames image files to the format of "imageXX.xxx"
+#>
+
+$index = 0
+$exts = "jpg", "jpeg", "png"
+$ext = "png"
+$files = Get-ChildItem *.png
+#     Foreach ($ext in $exts)
+#    {
+#        Write-Host "Looking at $ext file extension"
+        ForEach ($file in $files)
+        {
+            Write-host "Remaming file $file to image$index.$ext"
+            rename-item $file -NewName "image$index.$ext"
+            $index++    
+        }
+#    }
